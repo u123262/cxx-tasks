91ae5414ab92f98f727cfaf8dc2d81e4689dc0b5
Shipitsyn Kirill <36473355+u123262@users.noreply.github.com>
2018-11-06 23:05:54 +0300

    Update variant.hpp

--- variant/include/variant.hpp
+++ variant/include/variant.hpp
@@ -15,49 +15,76 @@
 #include <boost/variant.hpp>
 
 //{ number
-struct number
+struct number: std::variant<int, float>
 {
-
+    template<typename T>
+    number(const T& x):std::variant<int, float>(x) {};
 };
 //}
 
 //{ array
-struct array
+struct array: std::vector<number>
 {
-
+    array(std::initializer_list<number> x):std::vector<number>(x) {};
 };
 //}
 
 //{ recursive_array
-struct recursive_array
+struct recursive_array: std::vector<std::variant<number, std::shared_ptr<recursive_array>>>
 {
-
+    recursive_array(std::initializer_list<std::variant<number, std::shared_ptr<recursive_array>>> x):
+    std::vector<std::variant<number, std::shared_ptr<recursive_array>>>(x) {};
 };
 //}
 
 //{ recursive_array2
-struct recursive_array2
+struct recursive_array2: std::vector<std::variant<number, boost::recursive_wrapper<recursive_array2>>>
 {
+    auto convert(const std::initializer_list<std::variant<number, recursive_array2>>& input){
+		std::vector<std::variant<number, boost::recursive_wrapper<recursive_array2>>> tmp;
 
+		for(auto x : input) {
+			if (x.index() == 1)
+				tmp.push_back(boost::recursive_wrapper<recursive_array2>(std::get<recursive_array2>(x)));
+			else
+				tmp.push_back(std::get<number>(x));
+		}
+
+		return tmp;
+	}
+
+	recursive_array2(std::initializer_list<std::variant<number, recursive_array2>> x):
+	std::vector<std::variant<number, boost::recursive_wrapper<recursive_array2>>>(convert(x)) {};
 };
 //}
 
 //{ variant_decorator
-template<???>
-struct variant_decorator
+template<typename...T>
+struct variant_decorator: std::variant<T...>
 {
+    using base = std::variant<T...>;
+	using base::base;
 
-    ?? as()
-    {
+	template <typename T>
+	std::enable_if_t<!std::is_same<T, struct recursive_map>::value, T>
+		as() { return std::get<T>(*this); }
 
-    }
+	template <typename T>
+	std::enable_if_t<std::is_same<T, struct recursive_map>::value, T>
+		as() { return std::get<boost::recursive_wrapper< recursive_map >>(*this).get(); }
 };
 //}
 
 //{ recursive_map
-struct recursive_map
+struct recursive_map: std::unordered_map<
+	std::string,
+	variant_decorator< int, bool, std::string,  boost::recursive_wrapper< recursive_map > > >
 {
+    using base = std::unordered_map<
+			std::string,
+			variant_decorator< int, bool, std::string,  boost::recursive_wrapper< recursive_map > > >;
 
+	using base::base;
 };
 //}
 
